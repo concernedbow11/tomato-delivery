d20bf0ce6c6f896efc3acf66b96e93a0c932f444
Sahil Lukose <44937743+concernedbow11@users.noreply.github.com>
2022-12-19 21:25:30 +0000

    setting up
    setting up the app

---
+++ screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import { View, Text } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+
+  return (
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-red-400">HomeScreen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
